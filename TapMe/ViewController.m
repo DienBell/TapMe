495b28b89cf53fdb26940ce1401ac4e45b537444
DienBell <DienBell@gmail.com>
2015-01-03 19:49:52 +0800

    v0.0.1 first version

--- TapMe/ViewController.m
+++ TapMe/ViewController.m
@@ -14,14 +14,48 @@
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setupGame];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+-(IBAction)buttonPressed{
+    NSLog((@"Pressed!"));
+    count++;
+    
+    scoreLabel.text = [NSString stringWithFormat:@"Score\n%ld",count];
+}
+-(void)setupGame{
+    seconds = 30;
+    count = 0;
+    scoreLabel.text = [NSString stringWithFormat:@"Score\n%ld",count];
+    timerLabel.text = [NSString stringWithFormat:@"Time:%ld",seconds];
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(subtractTime) userInfo:nil repeats:YES];
+}
+-(void)subtractTime{
+    seconds --;
+    timerLabel.text = [NSString stringWithFormat:@"Time:%ld",seconds];
+    if(seconds == 0){
+        [timer invalidate];
+        
+        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"Time is up"
+                                                        message:[NSString stringWithFormat:@"Your score is:\n%ld",count]
+                                                       delegate:self
+                                              cancelButtonTitle:@"Play again"
+                                              otherButtonTitles:nil];
+        [alert show];
+    }
+}
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    [self setupGame];
 }
 
 @end
