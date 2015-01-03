0186e3ae77880cb6def4dc23ea42d1f9a3a95a6d
DienBell <DienBell@gmail.com>
2015-01-03 11:49:04 +0800

    Initial Commit

---
+++ TapMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TapMe
+//
+//  Created by bellnas on 15/1/3.
+//  Copyright (c) 2015年 DienBell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TapMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TapMe
+//
+//  Created by bellnas on 15/1/3.
+//  Copyright (c) 2015年 DienBell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
